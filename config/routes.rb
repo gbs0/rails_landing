c4b69c649ba23f64023fe08e23b2929dad8e6d95
gabriel schiavo <gabriel@lewagon.org>
2019-01-20 11:16:00 -0200

    add dashboard page

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root to: 'pages#home'
 
   get 'message', to: 'messages#index', as: :message
-  
+  get 'dashboard', to: 'pages#dashboard', as: :dashboard
 
   #resoures :message, only: [:show]   # ?
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
